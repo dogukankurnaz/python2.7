a0e3c076abd1ee6e7533449b7b8a9a9b9cab25a8
Doğukan KURNAZ <dogukankurnaz@gmail.com>
2021-02-16 04:49:23 +0300

    Update and rename Maaş Hesapla.py to MaasHesapla.py

---
+++ MaasHesapla.py
@@ -0,0 +1,6 @@
+while(True):
+    SicilNo = int(raw_input("Sicil Numaranizi Giriniz :"))
+    Calsa = int(raw_input("Çalisma saatinizi giriniz :"))
+    sauc = int(raw_input("Saat Ücretini Giriniz :"))
+    Maas = Calsa*sauc
+    print(Maas)
--- Maaş Hesapla.py
+++
@@ -1,6 +0,0 @@
-while(True):
-    SicilNo = int(raw_input("Sicil Numaranýzý Giriniz :"))
-    Calsa = int(raw_input("Çalýþma saatinizi giriniz :"))
-    sauc = int(raw_input("Saat Ücretini Giriniz :"))
-    Maas = Calsa*sauc
-    print(Maas)
